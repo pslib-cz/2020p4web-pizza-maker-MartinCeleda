c517efd31aaa5cd23bdd39e7a9468cceb2157469
Martin.Celeda <martin.celeda@pslib.cz>
2021-01-07 14:56:21 +0100

    router

---
+++ src/Components/NothingFound.jsx
@@ -0,0 +1,11 @@
+import { Jumbotron } from "reactstrap";
+
+const NothingFound = props =>{
+    return(
+        <Jumbotron className="mt-2">
+            <h1 className="display-4">404</h1>
+        </Jumbotron>
+    )
+}
+
+export default NothingFound;
